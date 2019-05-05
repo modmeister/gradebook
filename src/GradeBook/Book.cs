4f057d9a16f063d8525db168307ee765eedde694
modmeister <modmeister@protonmail.com>
2019-05-05 09:19:56 +0700

    Initial commit

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Book
+    {
+        private string name;
+        private List<double> grades;
+
+        public Book(string name)
+        {
+            this.name = name;
+            grades = new List<double>();
+        }
+
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        public void ShowStatistics()
+        {
+            var averageGrade = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+            foreach (var grade in grades)
+            {
+                highGrade = Math.Max(grade, highGrade);
+                lowGrade = Math.Min(grade, lowGrade);
+                averageGrade += grade;
+            }
+
+            if (grades.Count == 0)
+            {
+                averageGrade = 0.0;
+                highGrade = 0.0;
+                lowGrade = 0.0;
+            }
+            else
+            {
+                averageGrade /= grades.Count;
+            }
+
+            Console.WriteLine($"The highest grade is {highGrade:N1}");
+            Console.WriteLine($"The lowest grade is {lowGrade:N1}");
+            Console.WriteLine($"The average grade is {averageGrade:N1}");
+        }
+    }
+}
---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Khoa's Grade Book");
+            book.AddGrade(5.0);
+            book.AddGrade(7.0);
+            book.AddGrade(9.0);
+            book.ShowStatistics();
+        }
+    }
+}
